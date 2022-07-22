27050e28e680ba42374c92b1c714b5a98a4bb6ec
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-22 11:07:32 -0700

    removed irrelevant data

--- movieAPItest/testing.js
+++ movieAPItest/testing.js
@@ -1,16 +1,11 @@
 var zone = document.querySelector("#movieZone")
 
-var url = //"https://api.themoviedb.org/3/genre/movie/list?api_key=e346bd747060c7a18ce3926d8f5571b9&language=en-US"
-        "https://api.themoviedb.org/3/discover/movie?api_key=e346bd747060c7a18ce3926d8f5571b9&with_genres=18&page=1"
-        //  "http://openlibrary.org/subjects/genre.json"
+// get the list of genres and their corresponding IDs
+//"https://api.themoviedb.org/3/genre/movie/list?api_key=e346bd747060c7a18ce3926d8f5571b9&language=en-US"
+
+var url = "https://api.themoviedb.org/3/discover/movie?api_key=e346bd747060c7a18ce3926d8f5571b9&with_genres=18&page=1"
 
 var posterPath = "https://image.tmdb.org/t/p/original/"
-
-// var boardAPI = "https://api.boardgameatlas.com/api/search?name=Catan&pretty=true&client_id=rHFhdFeTrQ&categories=aliens"
-
-
-// var pastGenre = 'love'
-// url = url.replace('genre',pastGenre) 
 
 fetch(url)
     .then(function (response) {
